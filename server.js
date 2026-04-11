18b673657efd92503547cec98b2f36d341ba3ac6
Hiroyuki1313 <munozdiaza@gmail.com>
2026-04-11 13:05:16 -0600

    Fix: Force absolute path for .env in server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '.env') });
 const { createServer } = require('http');
 const { parse } = require('url');
 const next = require('next');
